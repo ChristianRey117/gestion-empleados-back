17f5d88df66811265249b921f99658107ef79c1b
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-28 01:34:34 -0400

    feat(employee-controller): post employees

--- src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
@@ -8,12 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gestion.empleados.gestion_empleados_back.model.Empleado;
 import com.gestion.empleados.gestion_empleados_back.repository.EmpleadoRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins="http://localhost:4200/")
 public class EmpleadoController {
 
     @Autowired
@@ -23,5 +33,11 @@ public class EmpleadoController {
     public List<Empleado> listEmpleados() {
         return _empleadoRepository.findAll();
     }
+
+    @PostMapping("/empleados")
+    public Empleado saveEmployees(@RequestBody Empleado employees) {        
+        return _empleadoRepository.save(employees);
+    }
+    
     
 }
