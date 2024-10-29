3a820aa3a8f54d406995539e84aab52f342f668e
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-29 10:39:06 -0400

    feat(empleado): add route to get employee by id

--- src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
@@ -3,10 +3,12 @@ package com.gestion.empleados.gestion_empleados_back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestion.empleados.gestion_empleados_back.model.Empleado;
+import com.gestion.empleados.gestion_empleados_back.model.Exceptions.ResourceNotFoundException;
 import com.gestion.empleados.gestion_empleados_back.repository.EmpleadoRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,6 +41,14 @@ public class EmpleadoController {
     public Empleado saveEmployees(@RequestBody Empleado employees) {        
         return _empleadoRepository.save(employees);
     }
+    
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> getEmpleado(@PathVariable Long id) {
+        Empleado employee = _empleadoRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No se encontro ningun empleado") );
+        return ResponseEntity.ok(employee);
+    }
+
+    
     
     
 }
