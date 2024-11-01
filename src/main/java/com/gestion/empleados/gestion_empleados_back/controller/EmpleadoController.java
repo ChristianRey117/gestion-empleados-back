8abae35922be86bd9f3e474a99580085808a094d
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-11-01 01:41:17 -0400

    feat(delete): add method to delete employee

--- src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
@@ -3,6 +3,7 @@ package com.gestion.empleados.gestion_empleados_back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,8 @@ import com.gestion.empleados.gestion_empleados_back.model.Exceptions.ResourceNot
 import com.gestion.empleados.gestion_empleados_back.repository.EmpleadoRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +23,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins="http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoController {
 
     @Autowired
@@ -40,24 +37,35 @@ public class EmpleadoController {
     }
 
     @PostMapping("/empleados")
-    public Empleado saveEmployees(@RequestBody Empleado employees) {        
+    public Empleado saveEmployees(@RequestBody Empleado employees) {
         return _empleadoRepository.save(employees);
     }
-    
+
     @GetMapping("/empleados/{id}")
     public ResponseEntity<Empleado> getEmpleado(@PathVariable Long id) {
-        Empleado employee = _empleadoRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No se encontro ningun empleado") );
+        Empleado employee = _empleadoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No se encontro ningun empleado"));
         return ResponseEntity.ok(employee);
     }
 
     @PutMapping("/empleados/{id}")
     public ResponseEntity<Empleado> updateEmployee(@PathVariable Long id, @RequestBody Empleado detailsEmployee) {
-        Empleado employee = _empleadoRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No se encontro ningun empleado") );
+        Empleado employee = _empleadoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No se encontro ningun empleado"));
         employee.setNombre(detailsEmployee.getNombre());
         employee.setApellido(detailsEmployee.getApellido());
         employee.setEmail(detailsEmployee.getEmail());
 
         Empleado employeeUpdated = _empleadoRepository.save(employee);
         return ResponseEntity.ok(employeeUpdated);
+    }
+
+    @DeleteMapping("/empleados/{id}")
+    public List<Empleado> deleteEmployee(@PathVariable long id) {
+        Empleado employee = _empleadoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not exist id: " + id));
+        _empleadoRepository.delete(employee);
+        List<Empleado> newListEmpleados = _empleadoRepository.findAll();
+        return newListEmpleados;
     }
 }
