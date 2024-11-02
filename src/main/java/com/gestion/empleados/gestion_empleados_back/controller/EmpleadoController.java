58a654c793b22671dc0b64661806ed00130f707a
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-11-02 12:03:53 -0400

    feat(deploy): add variables to deploy

--- src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/gestion_empleados_back/controller/EmpleadoController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin
 public class EmpleadoController {
 
     @Autowired
